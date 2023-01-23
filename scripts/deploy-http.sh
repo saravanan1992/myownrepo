795b14dd32291d61afb8d284837937e1bd1aee85
saravanan1992 <saravananelango1992@gmail.com>
2023-01-23 21:10:14 +0100

    Update deploy-http.sh

--- scripts/deploy-http.sh
+++ scripts/deploy-http.sh
@@ -1,1 +1,14 @@
 
+#! /bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SOURCE_DIR="${DIR}/../src"
+
+source "${DIR}/.env.local"
+
+gcloud functions \
+  deploy ${twitter_scrap} \
+  --source=${https://github.com/saravanan1992/myownrepo/tree/main/src} \
+  --runtime=python37 \
+  --trigger-http \
+  --allow-unauthenticated
